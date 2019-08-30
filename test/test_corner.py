bd3af81e7a67baf79cc4082ac50d3dd29ff6f70b
ZHYHenryZhang <henryzhang19951213@gmail.com>
2019-08-30 11:54:59 -0700

    corner test not implemented

---
+++ test/test_corner.py
@@ -0,0 +1,28 @@
+""" tests for the Corner class
+
+Author: Henry Zhang
+Date:August 30, 2019
+"""
+
+# module
+import matplotlib.pyplot as plt
+
+from src.simulator.car import Car, CarObject
+from src.sensor.corner import Corner
+
+# parameters
+
+
+# classes
+class Test_Corner():
+  pass
+
+# functions
+
+
+# main
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
