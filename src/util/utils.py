d9d8104c9c85e8002223e62f3de642cddeecf669
ZHYHenryZhang <henryzhang19951213@gmail.com>
2019-08-31 12:04:06 -0700

    util functions

---
+++ src/util/utils.py
@@ -0,0 +1,56 @@
+""" the utility functions
+
+Author: Henry Zhang
+Date:August 31, 2019
+"""
+
+# module
+import math
+
+# parameters
+
+
+# classes
+
+
+# functions
+def get_box_corners(x, y, orientation, l, w):
+  ''' return corner points of an oriented box '''
+  
+  l_2 = l / 2
+  w_2 = w / 2
+  yaw_cos = math.cos(orientation)
+  yaw_sin = math.sin(orientation)
+
+  front_left_x = x + l_2 * yaw_cos - w_2 * yaw_sin
+  front_left_y = y + l_2 * yaw_sin + w_2 * yaw_cos
+  front_right_x = x + l_2 * yaw_cos + w_2 * yaw_sin
+  front_right_y = y + l_2 * yaw_sin - w_2 * yaw_cos
+  back_left_x = x - l_2 * yaw_cos - w_2 * yaw_sin
+  back_left_y = y - l_2 * yaw_sin + w_2 * yaw_cos
+  back_right_x = x - l_2 * yaw_cos + w_2 * yaw_sin
+  back_right_y = y - l_2 * yaw_sin - w_2 * yaw_cos
+  front_mid_x = x + l_2 / 2 * yaw_cos
+  front_mid_y = y + l_2 / 2 * yaw_sin
+
+  return (front_left_x, front_left_y), (front_right_x, front_right_y), \
+         (back_left_x, back_left_y), (back_right_x, back_right_y), (front_mid_x, front_mid_y)
+
+def get_box_outline(x, y, orientation, l, w):
+  corners = get_box_corners(x, y, orientation, l, w)
+
+  outline = []
+  outline.append( ( (corners[0][0], corners[1][0]), (corners[0][1], corners[1][1]) ) )
+  outline.append( ( (corners[1][0], corners[3][0]), (corners[1][1], corners[3][1]) ) )
+  outline.append( ( (corners[3][0], corners[2][0]), (corners[3][1], corners[2][1]) ) )
+  outline.append( ( (corners[2][0], corners[0][0]), (corners[2][1], corners[0][1]) ) )
+  outline.append( ( (x, corners[4][0]), (y, corners[4][1]) ) )
+  return outline
+
+
+# main
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
