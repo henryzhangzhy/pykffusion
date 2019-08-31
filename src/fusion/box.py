841755073619f9192613e68d5b5f227feb66e0c4
ZHYHenryZhang <henryzhang19951213@gmail.com>
2019-08-31 12:03:30 -0700

    added box visualization

--- src/fusion/box.py
+++ src/fusion/box.py
@@ -5,22 +5,29 @@ Date:August 23, 2019
 """
 
 # module
+from matplotlib import pyplot as plt
 
+from src.util.utils import get_box_outline
 
 # parameters
 
 
 # classes
 class Box2D():
-  def __init__(self, x, y, yaw, omega, w, l, v=0, a=0):
+  def __init__(self, x, y, orientation, omega, w, l, v=0, a=0):
     self.x = x
     self.y = y
-    self.yaw = yaw
+    self.orientation = orientation
     self.omega = omega
     self.w = w
     self.l = l
     self.v = v
     self.a = a
+  
+  def viz(self):
+    outline = get_box_outline(self.x, self.y, self.orientation, self.l, self.w)
+    for line in outline:
+      plt.plot(line[0], line[1], c='b')
 
 # functions
 
